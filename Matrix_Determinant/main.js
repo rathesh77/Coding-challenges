8f36aac4f31649bd16627f91bdd9108d849c8b2c
Rathesh <rathesh.charles-mahendran>
2020-05-19 13:06:15 +0200

    fix edge cases

--- Matrix_Determinant/main.js
+++ Matrix_Determinant/main.js
@@ -1,41 +1,49 @@
 var valeurs = [];
 
 function determinant(m) {
+    console.table(m);
+
     if (m[0].length == 1)
         return parseInt(m[0])
 
     if (m[0].length == 2)
         return squaredMatrixDeterminant(m);
 
-    iterateMatrix(m, m[0][0]);
+    iterateMatrix(m);
 
-    var finalResult = valeurs.reduce((acc, currentVal) => acc + currentVal);
-    finalResult /= m[0][0];
-
+    var finalResult = valeurs.filter((w) => w != undefined).reduce((acc, currentVal) => acc + currentVal);
+   
     valeurs = [];
+
     return finalResult;
 };
 
-function squaredMatrixDeterminant(m) {
-    return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0]);
+function squaredMatrixDeterminant(m, val) {
+    if (m[0].length == 2) {
+        if (val == undefined)
+            return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0]);
+
+        return val * ((m[0][0] * m[1][1]) - (m[0][1] * m[1][0]));
+    }
+    valeurs.push(squaredMatrixDeterminant(getMatrix(1, 0, m), val * m[0][0]))
+    for (var i = 1; i < m[0].length; i++) {
+        if (i % 2 == 0)
+            valeurs.push(squaredMatrixDeterminant(getMatrix(1, i, m), val * m[0][i]))
+        else
+            valeurs.push(squaredMatrixDeterminant(getMatrix(1, i, m), -1 * val * m[0][i]))
+    }
 }
 
-function iterateMatrix(m, val) {
-    if (m.length < 2)
-        return;
-    if (m.length == 2) {
-        valeurs.push(val * squaredMatrixDeterminant(m));
-        return;
-    }
+function iterateMatrix(m) {
     for (var i = 0; i < m[0].length; i++) {
         if (i % 2 == 0)
-            iterateMatrix(getMat(1, i, m), val * m[0][i]);
+            valeurs.push(squaredMatrixDeterminant(getMatrix(1, i, m), m[0][i]))
         else
-            iterateMatrix(getMat(1, i, m), -1 * val * m[0][i]);
+            valeurs.push(squaredMatrixDeterminant(getMatrix(1, i, m), -1 * m[0][i]))
     }
 }
 
-function getMat(i, ignore, m) {
+function getMatrix(i, ignore, m) {
     var matrix = [];
     for (var k = i; k < m.length; k++) {
         matrix.push([]);
@@ -46,13 +54,19 @@ function getMat(i, ignore, m) {
     }
     return matrix;
 }
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max + 1 - (min)) + (min));
+}
 
-var currentTime = new Date().getTime();
+const currentTime = new Date().getTime();
 
-console.log(determinant([
+var matrice = []
+const dimension = getRandomNumberBetween(2, 6)
+for (var i = 0; i < dimension; i++) {
+    matrice.push([]);
+    for (var j = 0; j < dimension; j++)
+        matrice[matrice.length - 1].push(getRandomNumberBetween(-100, 100));
+}
 
-    [1, 2, 3, 4, 8], [5, 6, 7, 8, 0], [-9, 1, 2, 879, 3], [-55, 28, 19, 56, -77], [156, -456, 2, 1112, 9]
-
-]));
-
+console.log(determinant(matrice));
 console.log("\nTemps d'execution: " + (new Date().getTime() - currentTime) / 1000 + " secondes");
