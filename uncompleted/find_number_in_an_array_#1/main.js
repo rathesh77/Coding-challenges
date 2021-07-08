6338dc5b29eff4d3ff09bc18034cb394c9f6f7c6
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-07-09 01:08:11 +0200

    solved find_number_in_an_array_#1

--- uncompleted/find_number_in_an_array_#1/main.js
+++ uncompleted/find_number_in_an_array_#1/main.js
@@ -1,70 +1,15 @@
-
 function duplicateOrUnique(arr) {
-  //coding and coding..
+  let sum = 0
   const n = arr.length
+  const expr1 = (n - 1) * n
+  const expr2 = (n + 1) / 2
 
-  quicksort(arr, 0, arr.length-1)
-  console.log(arr)
+  for (let i = 0; i < n; i++)
+    sum += arr[i]
 
-  if (arr[n-1]  == n-1 ) {
-    // un seul element dupliqué !!!
-    for ( let i = 1 ; i < arr.length-1;i++) {
-      if ( arr[i] == arr[i+1] || arr[i] == arr[i-1]) {
-        return arr[i]
-      }
-    }
-  }
-  for ( let i = 1 ; i < arr.length-1;i++) {
-    if ( arr[i] != arr[i+1] && arr[i] != arr[i-1]) {
-      console.log(arr[i-1], arr[i],arr[i+1])
-      return arr[i]
-    }
-  }
-  return arr[n-1]
-
-}
-
-function quicksort(array, begin, end) {
-  if (begin < end) {
-      const j = partition(array, begin, end)
-      quicksort(array, begin, j - 1)
-      quicksort(array, j + 1, end)
-
-  }
-}
-
-function partition(array, begin, end) {
-  const pivotIndex = parseInt((begin + end) / 2)
-  let pivot = array[pivotIndex]
-
-  let i = begin
-  let j = end
-
-  swap(array, begin, pivotIndex)
-
-  while (i < j) {
-      while (array[i] <= pivot) {
-          i++
-      }
-      while (array[j] > pivot) {
-          j--
-      }
-      if (i < j) {
-          swap(array, i, j)
-      }
-  }
-  swap(array, begin, j)
-  return j
-}
-function swap(array, i, j) {
-  const temp = array[i]
-  array[i] = array[j]
-  array[j] = temp
+  return sum > (expr1) / 2 ? sum - ((expr1) / 2) : (expr2 * (expr2 + 1)) - sum
 }
 
 console.log(duplicateOrUnique([1, 2, 3, 1, 2, 3, 4]), 4)
-//console.log(duplicateOrUnique([9, 8, 7, 1, 2, 3, 9, 7, 1, 2, 3, 4, 4, 5, 5, 6, 6]), 8)
-//console.log(duplicateOrUnique([1, 2, 3, 6, 5, 4, 1]), 1)
-
-
-// 1 +2 +3 +4 = 10 => n(n+1) / 2 
+console.log(duplicateOrUnique([9, 8, 7, 1, 2, 3, 9, 7, 1, 2, 3, 4, 4, 5, 5, 6, 6]), 8)
+console.log(duplicateOrUnique([1, 2, 3, 6, 5, 4, 1]), 1)
