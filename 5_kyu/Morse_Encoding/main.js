257d8e6f78a05ce572bd27cc100e8612f2b640c7
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-07-26 18:50:24 +0200

    refactor

--- 5_kyu/Morse_Encoding/main.js
+++ 5_kyu/Morse_Encoding/main.js
@@ -11,9 +11,7 @@ Morse.invertBits = function (num) {
             invertedBits += '1'
         else
             invertedBits += '0'
-
-    if (invertedBits.length < 32)
-        invertedBits = '0'.repeat(32 - invertedBits.length) + invertedBits
+    invertedBits = '0'.repeat(32 - invertedBits.length) + invertedBits
 
     return invertedBits
 }
@@ -39,10 +37,9 @@ Morse.encode = function (message) {
         }
     }
     bits32 = bits32 + '0'.repeat(32 - bits32.length % 32)
-    if (bits32.length >= 32) {
-        twoComplement = (~parseInt(bits32.substring(0, 32), 2)) + 1
-        signedIntegers = signedIntegers.concat(-twoComplement)
-    }
+    twoComplement = (~parseInt(bits32.substring(0, 32), 2)) + 1
+    signedIntegers = signedIntegers.concat(-twoComplement)
+
     return signedIntegers
 };
 
@@ -64,10 +61,8 @@ Morse.decode = function (integerArray) {
                 chars = chars + (currentChar)
             }
         })
-
         chars = chars + ' '
     }
-
     return chars.trim()
 };
 
@@ -129,62 +124,9 @@ Morse.alpha = {
     ' ': '0' // Technically is 7 0-bits, but we assume that a space will always be between two other characters
 };
 
-Morse.beta = {
-    '10111': 'A',
-    '111010101': 'B',
-    '11101011101': 'C',
-    '1110101': 'D',
-    '1': 'E',
-    '101011101': 'F',
-    '111011101': 'G',
-    '1010101': 'H',
-    '101': 'I',
-    '1011101110111': 'J',
-    '111010111': 'K',
-    '101110101': 'L',
-    '1110111': 'M',
-    '11101': 'N',
-    '11101110111': 'O',
-    '10111011101': 'P',
-    '1110111010111': 'Q',
-    '1011101': 'R',
-    '10101': 'S',
-    '111': 'T',
-    '1010111': 'U',
-    '101010111': 'V',
-    '101110111': 'W',
-    '11101010111': 'X',
-    '1110101110111': 'Y',
-    '11101110101': 'Z',
-    '1110111011101110111': '0',
-    '10111011101110111': '1',
-    '101011101110111': '2',
-    '1010101110111': '3',
-    '10101010111': '4',
-    '101010101': '5',
-    '11101010101': '6',
-    '1110111010101': '7',
-    '111011101110101': '8',
-    '11101110111011101': '9',
-    '10111010111010111': '.',
-    '1110111010101110111': ',',
-    '101011101110101': '?',
-    '1011101110111011101': '\'',
-    '1110101110101110111': '!',
-    '1110101011101': '/',
-    '111010111011101': '(',
-    '1110101110111010111': ')',
-    '10111010101': '&',
-    '11101110111010101': ':',
-    '11101011101011101': ';',
-    '1110101010111': '=',
-    '1011101011101': '+',
-    '111010101010111': '-',
-    '10101110111010111': '_',
-    '101110101011101': '"',
-    '10101011101010111': '$',
-    '10111011101011101': '@',
-};
+Morse.beta = {};
+for (const entry of Object.entries(Morse.alpha))
+    Morse.beta[entry[1]] = entry[0]
 
 console.log(Morse.encode('HELLO WORLD'))
 console.log(Morse.decode([-1440552402, -1547992901, -1896993141, -1461059584]))
