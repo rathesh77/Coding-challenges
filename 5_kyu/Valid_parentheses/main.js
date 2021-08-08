947e4ae750407487e438444cbae5ff67af86557d
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-08-08 16:24:25 +0200

    fix previous bad solution for Valid_parentheses (5kyu)

--- 5_kyu/Valid_parentheses/main.js
+++ 5_kyu/Valid_parentheses/main.js
@@ -1,10 +1,14 @@
 function validParentheses(parens) {
-
-    let parentheses = new Array(2).fill(0);
-
-    for (let i = 0; i < parens.length; i++)
-        parentheses[parens[i].charCodeAt() - 40]++;
-
-    return parentheses[0] == parentheses[1] && parens[0] != ')' && parens[parens.length - 1] != '(' && parens != '())(()';
+    let count = 0
+    for (let i = 0; i < parens.length; i++) {
+        if (parens[i] === '(')
+            count++
+        else if (parens[i] === ')')
+            count--
+        if (count === -1)
+            break
+    }
+    return count === 0
 }
+
 validParentheses('())(()');
