771a69ff30d5f49c24df6ce6755ded485c0c333b
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-08-10 23:17:56 +0200

    refactor anagrams (5kyu)

--- 5_kyu/anagrams/main.js
+++ 5_kyu/anagrams/main.js
@@ -1,14 +1,15 @@
 function anagrams(word, words) {
 
-    var finalArray = [];
+    const finalArray = [];
+    const sortedWord = word.split('').sort().join('')
     // we iterate through the array of words
-    for (var i = 0; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++) {
         // we split each word to convert each of them to an array then we sort the array and compare with the splitted and sorted
         // word we have
         // eg : word  = 'ba' => ['b','a'] =>  ['a','d'] => 'ab'
         //      words[i] = 'da' => ['d','a'] =>  ['a','d'] => 'ad'
-        // 'ab' == 'ad' ? we push word[i] to the final array : we break
-        if (words[i].split('').sort().join('') != word.split('').sort().join(''))
+        // 'ab' == 'ad' ? we push word[i] to the final array : we skip
+        if (words[i].split('').sort().join('') != sortedWord)
             continue;
         finalArray.push(words[i])
     }
