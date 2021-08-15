4f114bceceb1908dbbd511887bedb5a4ed1195b8
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-08-15 23:05:03 +0200

    refactor sudoku_solver

--- 3_kyu/Sudoku_solver/main.js
+++ 3_kyu/Sudoku_solver/main.js
@@ -1,5 +1,5 @@
 function sudoku(puzzle) {
-    let filled = {}
+    const filled = {}
     for (let row = 0; row < puzzle.length; row++) {
         for (let col = 0; col < puzzle[row].length; col++) {
             if (!filled[row])
@@ -14,9 +14,7 @@ function sudoku(puzzle) {
     while (row != 9) {
         if (!filled[row][col]) {
             puzzle[row][col]++
-            while (!checkRow(puzzle[row][col], row, col, puzzle) ||
-                !checkCol(puzzle[row][col], row, col, puzzle) ||
-                !checkBox(puzzle[row][col], row, col, puzzle) && puzzle[row][col] < 10)
+            while (!checkIfCellIsValid(puzzle[row][col], row, col, puzzle) && puzzle[row][col] < 10)
                 puzzle[row][col]++
 
             if (puzzle[row][col] == 10) {
@@ -51,31 +49,20 @@ function sudoku(puzzle) {
     return puzzle
 }
 
-function checkRow(num, row, col, puzzle) {
-    for (let j = 0; j < 9; j++) {
-        if (j == col)
-            continue
-        if (puzzle[row][j] == num)
+function checkIfCellIsValid(num, row, col, puzzle) {
+    let i = 0
+    for (; i < 9; i++)
+        if (puzzle[row][i] == num && i != col)
             return false
-    }
-    return true
-}
 
-function checkCol(num, row, col, puzzle) {
-    for (let i = 0; i < 9; i++) {
-        if (i == row)
-            continue
-        if (puzzle[i][col] == num)
+    for (i = 0; i < 9; i++)
+        if (puzzle[i][col] == num && i != row)
             return false
-    }
-    return true
-}
 
-function checkBox(num, row, col, puzzle) {
-    let nthRow = Math.floor(row / 3)
-    let endRow = (nthRow * 3) + 3
-    let nthCol = Math.floor(col / 3)
-    let endCol = (nthCol * 3) + 3
+    const nthRow = Math.floor(row / 3)
+    const endRow = (nthRow * 3) + 3
+    const nthCol = Math.floor(col / 3)
+    const endCol = (nthCol * 3) + 3
 
     for (let r = nthRow * 3; r < endRow; r++) {
         for (let c = nthCol * 3; c < endCol; c++) {
