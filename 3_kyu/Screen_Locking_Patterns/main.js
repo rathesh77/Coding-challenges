a400ba59cc29e3a769e26c687165273fd1017de5
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-08-26 00:55:56 +0200

    refactor previous solution (Screen_Locking_Patterns 3kyu)

--- 3_kyu/Screen_Locking_Patterns/main.js
+++ 3_kyu/Screen_Locking_Patterns/main.js
@@ -4,33 +4,39 @@ class UndirectedGraph {
     }
     addNode(dot) {
         const alreadyRegisteredNode = this.nodes.get(dot)
-        if (alreadyRegisteredNode != null)
+        if (alreadyRegisteredNode)
             return alreadyRegisteredNode
 
-        const node = new _Node(dot)
+        const node = new Node(dot)
         this.nodes.set(node.value, node)
         return node
     }
     connectNodes(n1, n2) {
         const src = this.nodes.get(n1)
         const dest = this.nodes.get(n2)
-        if (src == null || dest == null)
+        if (!src || !dest)
             return
 
         src.addNext(dest)
         dest.addNext(src)
         return [src, dest]
     }
+
+    connectGroupsOfNodes(groups) {
+        for (const group of groups)
+            for (const target of group[1])
+                this.connectNodes(group[0], target)
+    }
 }
 
-class _Node {
+class Node {
     constructor(value) {
         this.value = value
         this.previous = new Map()
         this.next = new Map()
     }
     addNext(node) {
-        if (!node instanceof _Node)
+        if (!node instanceof Node)
             return
         this.next.set(node.value, node)
         node.previous.set(this.value, this)
@@ -38,117 +44,54 @@ class _Node {
     }
 }
 
-const g = new UndirectedGraph()
+const graph = new UndirectedGraph()
 const dots = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+const adjacentNodes = [
+    ['A', ['B', 'D', 'E', 'H', 'F']],
+    ['B', ['C', 'D', 'E', 'F', 'I', 'G']],
+    ['C', ['E', 'F', 'H', 'D']],
+    ['D', ['E', 'G', 'H', 'I']],
+    ['E', ['G', 'H', 'F', 'I']],
+    ['H', ['F', 'I', 'G']],
+    ['F', ['I', 'G']],
+]
+
 for (const dot of dots)
-    g.addNode(dot)
+    graph.addNode(dot)
 
-g.connectNodes('A', 'B')
-g.connectNodes('A', 'D')
-g.connectNodes('A', 'E')
-g.connectNodes('A', 'H')
-g.connectNodes('A', 'F')
-
-g.connectNodes('B', 'C')
-g.connectNodes('B', 'D')
-g.connectNodes('B', 'E')
-g.connectNodes('B', 'F')
-g.connectNodes('B', 'I')
-g.connectNodes('B', 'G')
-
-g.connectNodes('C', 'E')
-g.connectNodes('C', 'F')
-g.connectNodes('C', 'H')
-g.connectNodes('C', 'D')
-
-g.connectNodes('D', 'E')
-g.connectNodes('D', 'G')
-g.connectNodes('D', 'H')
-g.connectNodes('D', 'I')
-
-g.connectNodes('E', 'G')
-g.connectNodes('E', 'H')
-g.connectNodes('E', 'F')
-g.connectNodes('E', 'I')
-
-g.connectNodes('H', 'F')
-g.connectNodes('H', 'I')
-g.connectNodes('H', 'G')
-
-g.connectNodes('I', 'F')
-
-g.connectNodes('G', 'F')
+graph.connectGroupsOfNodes(adjacentNodes)
 
 function countPatternsFrom(firstPoint, length) {
     // Your code here
     if (length <= 0 || length > 9)
         return 0
-
-    let patterns = findPatterns(g, firstPoint, length - 1, {})
+    const patterns = findPatterns(firstPoint, length - 1, {})
     return !Array.isArray(patterns) ? 1 : patterns.length
 }
 
-function findPatterns(g, startingPoint, length, alreadyUsedDots) {
-
+function findPatterns(startingPoint, length, alreadyUsedDots) {
     if (length == 0)
         return alreadyUsedDots
-
+    const passingOverDots = {
+        'G': { 'D': 'A', 'E': 'C', 'H': 'I' },
+        'D': { 'E': 'F' },
+        'A': { 'B': 'C', 'E': 'I', 'D': 'G' },
+        'B': { 'E': 'H' },
+        'H': { 'E': 'B' },
+        'C': { 'B': 'A', 'E': 'G', 'F': 'I' },
+        'F': { 'E': 'D' },
+        'I': { 'H': 'G', 'F': 'C', 'E': 'A' }
+    }
     alreadyUsedDots[startingPoint] = true
     let count = []
-    for (const dot of g.nodes.get(startingPoint).next.keys()) {
+    for (const dot of graph.nodes.get(startingPoint).next.keys()) {
         if (!alreadyUsedDots[dot]) {
-            count = count.concat(findPatterns(g, dot, length - 1, { ...alreadyUsedDots }))
+            count = count.concat(findPatterns(dot, length - 1, { ...alreadyUsedDots }))
         } else {
-            let patterns = null
-
-            if (startingPoint == 'G') {
-                if (dot == 'D' && !alreadyUsedDots['A'])
-                    patterns = findPatterns(g, 'A', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'E' && !alreadyUsedDots['C'])
-                    patterns = findPatterns(g, 'C', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'H' && !alreadyUsedDots['I'])
-                    patterns = findPatterns(g, 'I', length - 1, { ...alreadyUsedDots })
-            }
-            else if (startingPoint == 'D' && dot == 'E' && !alreadyUsedDots['F'])
-                patterns = findPatterns(g, 'F', length - 1, { ...alreadyUsedDots })
-
-            else if (startingPoint == 'A') {
-                if (dot == 'B' && !alreadyUsedDots['C'])
-                    patterns = findPatterns(g, 'C', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'E' && !alreadyUsedDots['I'])
-                    patterns = findPatterns(g, 'I', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'D' && !alreadyUsedDots['G'])
-                    patterns = findPatterns(g, 'G', length - 1, { ...alreadyUsedDots })
-            }
-            else if (startingPoint == 'B' && dot == 'E' && !alreadyUsedDots['H'])
-                patterns = findPatterns(g, 'H', length - 1, { ...alreadyUsedDots })
-            else if (startingPoint == 'H' && dot == 'E' && !alreadyUsedDots['B'])
-                patterns = findPatterns(g, 'B', length - 1, { ...alreadyUsedDots })
-
-            else if (startingPoint == 'C') {
-                if (dot == 'B' && !alreadyUsedDots['A'])
-                    patterns = findPatterns(g, 'A', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'E' && !alreadyUsedDots['G'])
-                    patterns = findPatterns(g, 'G', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'F' && !alreadyUsedDots['I'])
-                    patterns = findPatterns(g, 'I', length - 1, { ...alreadyUsedDots })
-            }
-            else if (startingPoint == 'F' && dot == 'E' && !alreadyUsedDots['D'])
-                patterns = findPatterns(g, 'D', length - 1, { ...alreadyUsedDots })
-
-            else if (startingPoint == 'I') {
-                if (dot == 'H' && !alreadyUsedDots['G'])
-                    patterns = findPatterns(g, 'G', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'F' && !alreadyUsedDots['C'])
-                    patterns = findPatterns(g, 'C', length - 1, { ...alreadyUsedDots })
-                else if (dot == 'E' && !alreadyUsedDots['A'])
-                    patterns = findPatterns(g, 'A', length - 1, { ...alreadyUsedDots })
-            }
-            if (patterns != null)
-                count = count.concat(patterns)
-
+            const pod = passingOverDots[startingPoint]
+            if (pod && pod[dot] && !alreadyUsedDots[pod[dot]])
+                count = count.concat(findPatterns(pod[dot], length - 1, { ...alreadyUsedDots }))
         }
-
     }
     return count
 }
