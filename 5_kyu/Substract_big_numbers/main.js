065d1735e87525bffb59e7e12be25fde24ba1a86
CHARLES MAHENDRAN Ratesh <charlesmahendr@et.intechinfo.fr>
2021-08-26 21:59:49 +0200

    refactor Substract_big_numbers (5kyu)

--- 5_kyu/Substract_big_numbers/main.js
+++ 5_kyu/Substract_big_numbers/main.js
@@ -1,15 +1,14 @@
 function subtract(x, y) {
 
     let negative = false
-
     let i = 0
-    while (x[i] == '0') {
+    while (x[i] == '0')
         x = x.substring(i + 1)
-    }
+
     i = 0
-    while (y[i] == '0') {
+    while (y[i] == '0')
         y = y.substring(i + 1)
-    }
+
     if (+x <= +y) {
         const temp = x
         negative = true
@@ -17,46 +16,43 @@ function subtract(x, y) {
         y = temp
     }
 
-    let a = ((x >= 0 ? x : -x) + '').split('').reverse()
-    let b = ((y >= 0 ? y : -y) + '').split('').reverse()
+    let a = ((x >= 0 ? x : -x) + '')
+    let b = ((y >= 0 ? y : -y) + '')
 
-    i = 0
+
+    i = b.length - 1
+    let j = a.length - 1
     let carry = 0
     let result = ''
     while (b[i] != null) {
-
-        if (+b[i] + carry > +a[i]) {
-            result = ((10 + +a[i]) - (+b[i] + carry)) + result
+        if (+b[i] + carry > +a[j]) {
+            result = ((10 + +a[j]) - (+b[i] + carry)) + result
             carry = 1
         }
         else {
-            result = (+a[i] - (+b[i] + carry)) + result
+            result = (+a[j] - (+b[i] + carry)) + result
             carry = 0
         }
-        i++
+        i--
+        j--
     }
-    while (a[i] != null) {
-        if (+a[i] < carry) {
-            result = ((10 + +a[i]) - carry) + result
+    while (a[j] != null) {
+        if (a[j] < carry) {
+            result = ((10 + a[j]) - carry) + result
             carry = 1
         }
         else {
-            result = (+a[i] - carry) + result
+            result = (a[j] - carry) + result
             carry = 0
         }
-        i++
-    }
+        j--
 
-    i = 0
-    while (result[i] == '0') {
-        result = result.substring(i + 1)
     }
-    if (result[0] == 0)
+    while (result[0] == '0')
         result = result.substring(1)
     if (result == '')
         return '0'
     if (negative)
         result = '-' + result
-
     return result
 }
