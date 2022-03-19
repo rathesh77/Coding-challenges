33f2852675b87430f97399cc143633cd7cade874
rathesh77 <rathesh.charles@outlook.fr>
2022-03-19 20:16:38 +0100

    update SQL engine (2kyu) [WIP]

--- 3_kyu/Simple_SQL_engine/main.js
+++ 3_kyu/Simple_SQL_engine/main.js
@@ -33,26 +33,63 @@ function SQLEngine(database) {
 
     tree['SELECT'] = this.parseSelect(tree['SELECT'])
     tree['FROM'] = this.parseFrom(tree['FROM'])
-
     if (tree['WHERE'] != null)
       tree['WHERE'] = this.parseWhere(tree['WHERE'])
+
     console.log(tree)
+
     const firstTable = tree['FROM'].shift().table
+    console.log(firstTable)
+
     let resultSet = this.transformPropertiesOfTable(database[firstTable], firstTable)
 
+    // ------------ FROM TABLES
     let from = tree['FROM']
     for (let i = 0; i < from.length; i++) {
       const entry = from[i]
       if (entry.type == 'join') {
         resultSet = this.filterJoinType(resultSet, this.transformPropertiesOfTable(database[entry.table], entry.table), entry.condition)
+      } else if (entry.type == 'simple') {
+        resultSet = this.cartesianProduct(resultSet, entry.table)
       }
-      break
     }
-    // console.log(database[firstTable])
+
+    if (tree['WHERE'] != null) {
+      // ------------ WHERE CONDITION
+      let updatedSet = []
+      for (let i = 0; i < resultSet.length; i++) {
+        const eval = this.evalWhere(tree['WHERE'], resultSet[i])
+        if (eval == true) {
+          updatedSet.push(resultSet[i])
+        }
+      }
+      resultSet = updatedSet
+    }
+    // ------------ SELECT COLUMNS
+    resultSet.map(el => {
+      Object.keys(el).forEach(key => {
+        if (tree['SELECT'].indexOf(key) == -1) {
+          delete el[key]
+        }
+      })
+    })
     return resultSet
   }
 
+  this.cartesianProduct = function (resultSet, tablename) {
+    let out = []
+    const dest = database[tablename]
+    for (let i = 0; i < resultSet.length; i++) {
+      const left = resultSet[i]
+      for (let j = 0; j < dest.length; j++) {
+        const right = dest[i]
+        dest.push({ ...left, ...right })
+      }
+    }
+    return out
+  }
   this.transformPropertiesOfTable = function (obj, table) {
+    console.log(obj)
     return obj.map((el) => {
       let keys = Object.keys(el)
       let newObj = {}
@@ -63,8 +100,8 @@ function SQLEngine(database) {
     })
   }
   this.filterJoinType = function (resultSet, destTable, condition) {
-    console.log(destTable)
     let updatedSet = []
+    console.log(condition)
     for (let i = 0; i < resultSet.length; i++) {
       const leftRow = resultSet[i]
       for (let j = 0; j < destTable.length; j++) {
@@ -78,20 +115,46 @@ function SQLEngine(database) {
     return updatedSet
   }
 
-  this.evalCondition = function (condition, sourceRow, destRow) {
+  this.evalWhere = function (condition, row) {
     let { left, operator, right } = condition
     let leftIsColumnId = left.split('.')
     let rightIsColumnId = right.split('.')
     if (leftIsColumnId.length == 2) {
-      left = sourceRow[left]
-      if (!left)
-        left = destRow[right]
+      left = row[left]
     }
     if (rightIsColumnId.length == 2) {
-      right = destRow[right]
-      if (!right)
-        right = destRow[right]
+      right = row[right]
     }
+    return this.comparison(left, operator, right)
+
+  }
+
+  this.evalCondition = function (condition, sourceRow, destRow) {
+    let { left, operator, right } = condition
+    let leftIsColumnId = left.split('.')
+    let rightIsColumnId = right.split('.')
+
+
+    if (leftIsColumnId.length == 2) {
+      const temp = left
+      left = sourceRow[left]
+      if (left == null)
+        left = destRow[temp]
+    }
+    if (rightIsColumnId.length == 2) {
+      const temp = right
+
+      right = destRow[right]
+      if (right== null)
+        right = sourceRow[temp]
+    }
+
+   // console.log(left, right, sourceRow, destRow)
+
+    return this.comparison(left, operator, right)
+  }
+
+  this.comparison = function (left, operator, right) {
     if (operator == '=') {
       return left == right
     }
@@ -111,7 +174,6 @@ function SQLEngine(database) {
       return left != right
     }
   }
-
   this.parseSelect = function (fields) {
     return fields.split(',').map(el => el.trim())
   }
@@ -120,7 +182,7 @@ function SQLEngine(database) {
     tables = tables.split(',')
     for (let part of tables) {
       if (!part.includes('JOIN')) {
-        joins.push(part)
+        joins.push({ type: 'simple', table: part })
         continue
       }
       let firstTable = part.substr(0, part.indexOf(' '))
@@ -151,6 +213,9 @@ function SQLEngine(database) {
   }
 }
 
-const request = `SELECT movie.name, actor.name FROM movie JOIN actor_to_movie ON actor_to_movie.movieID = movie.id JOIN actor ON actor_to_movie.actorID = actor.id WHERE actor.name <> 'Daniel Craig'`
+//const request = `SELECT movie.name, actor.name FROM movie JOIN actor_to_movie ON actor_to_movie.movieID = movie.id JOIN actor ON actor_to_movie.actorID = actor.id WHERE actor.name <> 'Daniel Craig'`
+//const request = `SELECT movie.name FROM movie`
+//const request = `SELECT movie.name, director.name FROM movie JOIN director ON director.id = movie.directorID`
+const request = `select director.name from movie join actor_to_movie ON actor_to_movie.movieID = movie.id JOIN director on director.id = movie.directorID join actor on actor.id = actor_to_movie.actorID where movie.year <> 1994`
 const engine = new SQLEngine(database)
 console.log(engine.execute(request))
